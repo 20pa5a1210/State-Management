26ef61ed3982beced2224c4851109a46e069f372
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-04-07 11:30:53 +0530

    New Bracnhes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+function list() {
+  const [list, setList] = useState(["jack", "Jill"]);
+  const [name, setName] = useState(() => "Jack");
+
+  function AddName() {
+    setList([...list, name]);
+    setName("");
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  function addone() {
+    setCount(count + 1);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={addone}>count = {count}</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
